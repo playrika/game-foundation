75c80be97c571f43e29e111336ab71a477ce228e
Sergey Chemodanov <se.chemodanov@gmail.com>
2020-08-28 23:24:42 +0300

    Added NewtonsoftJsonConverter

---
+++ Runtime/Json/NewtonsoftJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿#if GF_NEWTONSOFT_JSON
+using Newtonsoft.Json;
+
+namespace Playrika.GameFoundation.Json
+{
+    public class NewtonsoftJsonConverter : IJsonConverter
+    {
+        public string SerializeObject(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public T DeserializeObject<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
+#endif
