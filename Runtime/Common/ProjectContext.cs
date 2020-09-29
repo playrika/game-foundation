9e25aa7a4d487fa6746c9899bacc8e7846295aed
Sergey Chemodanov <se.chemodanov@gmail.com>
2020-09-29 22:09:51 +0300

    Renamed ServiceLocator to ProjectContext

--- Runtime/Common/ProjectContext.cs
+++ Runtime/Common/ProjectContext.cs
@@ -3,40 +3,40 @@ using System.Collections.Generic;
 
 namespace Playrika.GameFoundation.Common
 {
-    public static class ServiceLocator
+    public static class ProjectContext
     {
-        private static readonly Dictionary<Type, object> _services = new Dictionary<Type, object>();
+        private static readonly Dictionary<Type, object> _bindings = new Dictionary<Type, object>();
 
         public static void Bind<T>(T service)
         {
             var type = typeof(T);
 
-            if (_services.ContainsKey(type))
+            if (_bindings.ContainsKey(type))
             {
-                _services[type] = service;
+                _bindings[type] = service;
                 return;
             }
 
-            _services.Add(typeof(T), service);
+            _bindings.Add(typeof(T), service);
         }
 
         public static void Unbind<T>()
         {
             var type = typeof(T);
-            _services.Remove(type);
+            _bindings.Remove(type);
         }
 
         public static void UnbindAll()
         {
-            _services.Clear();
+            _bindings.Clear();
         }
 
         public static T Get<T>() where T : class
         {
             var type = typeof(T);
 
-            if (_services.ContainsKey(type))
-                return (T)_services[type];
+            if (_bindings.ContainsKey(type))
+                return (T)_bindings[type];
 
             return null;
         }
